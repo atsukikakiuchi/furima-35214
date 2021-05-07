77fee7173132bcb46d3e69a2de29846fab3a42f7
atsukikakiuchi <akak92000@gmail.com>
2021-05-07 18:10:19 +0900

    修正　コミット漏れ

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index,:show]
   before_action :set_item, only: [:show, :edit, :update]
-  
+  before_action :move_to_index, only: [:edit]
   def index
     @items = Item.all.order("created_at DESC")
   end
@@ -43,5 +43,10 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def move_to_index
+    if user_signed_in? && @item.user_id != current_user.id
+      redirect_to root_path
+    end
+  end
 end
 
