d645bd251edeb813b663007fb3abe0811ccd98c7
atsukikakiuchi <akak92000@gmail.com>
2021-05-12 19:11:57 +0900

    購入機能

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -9,27 +9,28 @@ class PaymentsController < ApplicationController
   end
 
   def create
-
+    @item = Item.find(params[:item_id])
     @payment_address = PaymentAddress.new(payment_params)
+    binding.pry
     if @payment_address.valid?
       pay_item
       @payment_address.save
       redirect_to root_path
     else
-      render :new
+      render :index
     end
   end
 
   private
 
   def payment_params
-    params.require(:payment_address).permit(:postal_code, :prefecture_id, :manicipality, :house_number, :building_name, :phone_number) .merge(user_id: current_user.id)
+    params.require(:payment_address).permit(:postal_code, :prefecture_id, :manicipality, :house_number, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
   end
 
   def pay_item
     Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
     Payjp::Charge.create(
-      amount: payment_params[:price],
+      amount: @item.price,
       card: payment_params[:token],
       currency: 'jpy'
     )
--- app/models/payment_address.rb
+++ app/models/payment_address.rb
@@ -1,9 +1,7 @@
 class PaymentAddress
   include ActiveModel::Model
-  attr_accessor :postal_code, :prefecture_id, :manicipality, :house_number, :building_name, :phone_number, :payment, :item, :user, :token
+  attr_accessor :postal_code, :prefecture_id, :manicipality, :house_number, :building_name, :phone_number, :item_id, :user_id, :token, :payment_id
 
-  extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :prefecture
 
   with_options presence: true do
     validates :postal_code
@@ -11,13 +9,12 @@ class PaymentAddress
     validates :manicipality
     validates :house_number
     validates :phone_number
-    validates :payment
-    validates :item
-    validates :user
+    validates :item_id
+    validates :user_id
     validates :token
   end
   def save
-    payment = Payment.create(item: item, user: user)
-    Address.create(postal_code: postal_code, prefecture_id: prefecture_id, manicipality: manicipality, house_number: house_number, building_name: building_name, phone_number: phone_number, payment: payment)
+    payment = Payment.create(item_id: item_id, user_id: user_id)
+    Address.create(postal_code: postal_code, prefecture_id: prefecture_id, manicipality: manicipality, house_number: house_number, building_name: building_name, phone_number: phone_number, payment_id: payment.id)
   end
 end
