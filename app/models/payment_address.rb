d2cc509b9af578b4049931cc318cdde9e8240e52
atsukikakiuchi <akak92000@gmail.com>
2021-05-13 17:40:21 +0900

    テストコード

--- app/models/payment_address.rb
+++ app/models/payment_address.rb
@@ -5,10 +5,10 @@ class PaymentAddress
 
   with_options presence: true do
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
-    validates :prefecture_id
+    validates :prefecture_id, numericality: { other_than: 1 }
     validates :manicipality
     validates :house_number
-    validates :phone_number, format: {with: /\A[0-9]{3}-[0-9]{4}\z/}
+    validates :phone_number, Length: {maximum: 11 }
     validates :item_id
     validates :user_id
     validates :token
--- db/schema.rb
+++ db/schema.rb
@@ -38,7 +38,7 @@ ActiveRecord::Schema.define(version: 2021_05_10_095534) do
     t.integer "prefecture_id", null: false
     t.string "manicipality", null: false
     t.string "house_number", null: false
-    t.string "building_name", null: false
+    t.string "building_name"
     t.string "phone_number", null: false
     t.bigint "payment_id", null: false
     t.datetime "created_at", precision: 6, null: false
